c02896664afdbccecf6664b4cf28bfdbbf9045ca
escowin <edwin@escowinart.com>
2023-02-25 21:12:29 -0600

    popular | modularized styles & mapped trending placeholders into jsx

--- client/src/components/Popular/index.js
+++ client/src/components/Popular/index.js
@@ -1,93 +1,53 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-
-import ImgSecurity from "../../assets/images/cctv.jpeg";
-import ImgTV from "../../assets/images/tv.jpeg";
-import ImgWiFi from "../../assets/images/wifi.jpeg";
-import ImgLights from "../../assets/images/lights.webp";
-import Imggd from "../../assets/images/gd.jpeg";
-import Imgphone from "../../assets/images/phone.jpeg";
+import "./index.css";
 
 const Popular = () => {
   const popular = [
     {
       name: "wifi optimization",
-      img: "wifi.jpeg"
-    }
-  ]
+      img: "wifi.jpeg",
+    },
+    {
+      name: "Smart home lighting",
+      img: "lights.webp",
+    },
+    {
+      name: "Smart phone repair",
+      img: "phone.jpeg",
+    },
+    {
+      name: "Home theater",
+      img: "tv.jpeg",
+    },
+    {
+      name: "Smart garage",
+      img: "gd.jpeg",
+    },
+    {
+      name: "Home security",
+      img: "cctv.jpeg",
+    },
+  ];
 
   return (
     <>
-      <h2>Trending services</h2>
-      <article className="card-container">
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
+      <h2 className="title">Trending services</h2>
+
+      <section className="trending">
+        {popular.map((installation, i) => (
+          <article key={i} className="trending-item">
+            <Link to="/CreateProject" className="link">{installation.name}</Link>
             <img
-                src={ImgWiFi}
-                alt="Graph of Energy saving habits."
-                width={180}
-                height={180}
-            ></img>
-            <h4 className="card-title">Wifi Optimization</h4>
-          </Link>
-        </div>
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
-            <img
-              src={ImgLights}
-              alt="Graph of Energy saving habits."
-              width={180}
-              height={180}
-            ></img>{" "}
-            <h4 className="card-title">Smart Home Lighting</h4>
-          </Link>
-        </div>
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
-            <img
-              src={Imgphone}
-              alt="Graph of Energy saving habits."
-              width={180}
-              height={180}
-            ></img>
-            <h4 className="card-title">Smart Phone Repair</h4>
-          </Link>
-        </div>
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
-            <img
-              src={ImgTV}
-              alt="Graph of Energy saving habits."
-              width={180}
-              height={180}
-            ></img>
-            <h4 className="card-title">Home Theater</h4>
-          </Link>
-        </div>
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
-            <img
-              src={Imggd}
-              alt="Graph of Energy saving habits."
-              width={180}
-              height={180}
-            ></img>
-            <h4 className="card-title">Smart Garage </h4>
-          </Link>
-        </div>
-        <div className="project-card">
-          <Link to="/CreateProject" className="card" style={{ width: "18rem" }}>
-            <img
-              src={ImgSecurity}
-              alt="Graph of Energy saving habits."
-              width={180}
-              height={180}
-            ></img>
-            <h4 className="card-title">Home Security</h4>
-          </Link>
-        </div>
-      </article>
+              src={require(`../../assets/images/${installation.img}`)}
+              alt={installation.name}
+              className="trending-img"
+            />
+          </article>
+        ))}
+        ;
+      </section>
     </>
   );
 };
